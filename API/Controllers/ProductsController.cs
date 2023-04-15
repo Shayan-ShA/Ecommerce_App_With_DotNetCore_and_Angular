c3dd45d2ea528bfc2d42aa6cae39e7a9a9635986
Shawn Sheikhakbari <sh.sheikhakbari@gmail.com>
2023-04-15 14:37:18 -0500

    EWA#API_1.0

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Specification;
+using API.Dtos;
+using AutoMapper;
+using API.Errors;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> productsRepo,
+        IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType>
+        productTypeRepo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
+            _productsRepo = productsRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var products = await _productsRepo.ListAsync(spec);
+
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if(product == null) return NotFound(404);
+
+            return _mapper.Map<Product,ProductToReturnDto>(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepo.ListAllAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productTypeRepo.ListAllAsync());
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Core.Interfaces;
+using API.Helpers;
+using API.Middleware;
+using API.Extensions;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnections")));
+            
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+  
+            app.UseStatusCodePagesWithReExecute("error/{0}");
+
+            app.UseHttpsRedirection();
+
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            
+            app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
